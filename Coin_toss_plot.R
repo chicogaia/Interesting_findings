32a103a8c110df10f6b690d95fb100c9bfd1ca09
chicogaia <francisco.r.gaia@gmail.com>
2020-10-30 14:45:58 -0300

    Adicionada segunda função - fica uma para fair, uma para fair + unfair

--- Coin_toss_plot.R
+++ Coin_toss_plot.R
@@ -1,5 +1,36 @@
 
-coin.un_fair <- function(sample.size = 100 , samples.n = 10000 , prob_unfair = c(0.8, 0.2)) {
+coin.plot <- function(sample.size = 100 , samples.n = 10000 , prob = c(0.5, 0.5)) {
+        
+        #Generate vector for possible outcomes
+        coin <- c("heads", "tails")
+        
+        #Generate vector to store outcome = heads of the sample.size number of tosses
+        heads <- c()
+        
+        #Generate samples.n number of sample.size tosses and stores the number of heads outcome
+        for (i in 1:samples.n) {
+                coin.toss <- sample(coin, sample.size, replace = TRUE, prob)
+                heads[i] <- sum(coin.toss == "heads")
+        }
+        
+        #Generate .jpg file
+        setwd("C:/Users/Francisco Gaia/Documents/interesting_findings/LinkedIn Articles/Coin Toss pvalue")
+        jpeg("cointoss.jpeg", height = 600 , width = 800)
+        
+        
+        #Plot the number of heads outcome by frequency 
+        p.fair <- hist(heads, breaks = 15, col=rgb(0,0,1,1/3),
+                       xlim = c(20,80), ylim = c(0,2000) ,
+                       main = paste("Distribuição de ", samples.n, "amostras de ", sample.size , " jogadas de moeda"),
+                       xlab = "Número de vezes que caiu em 'Cara' ",
+                       ylab = "Número de amostras")
+        text(x = 70 , y = 1750, "Simulação em R por Francisco Gaia")
+        
+        dev.off()
+}   
+
+
+coin.plot2 <- function(sample.size = 100 , samples.n = 10000 , prob_unfair = c(0.8, 0.2)) {
   
   #Generate vector for possible outcomes
   coin <- c("heads", "tails")
@@ -30,7 +61,7 @@ coin.un_fair <- function(sample.size = 100 , samples.n = 10000 , prob_unfair = c
   
   #Generate .jpg file
   setwd("C:/Users/Francisco Gaia/Documents/interesting_findings/LinkedIn Articles/Coin Toss pvalue")
-  jpeg("cointoss.jpeg", height = 600 , width = 800)
+  jpeg("cointoss2.jpeg", height = 600 , width = 800)
 
  
   #Plot the number of heads outcome by frequency 
@@ -52,4 +83,7 @@ coin.un_fair <- function(sample.size = 100 , samples.n = 10000 , prob_unfair = c
   dev.off()
        
 }   
+
+
+
 
